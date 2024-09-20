7db07319e5c9c35d55e61e02d0934d7067fd836c
Ilayaraja2005 <ilayarajag2005@gmail.com>
2024-09-20 10:01:41 +0530

    Add files via upload

---
+++ CCC TRAINING/main.py
@@ -0,0 +1,14 @@
+# pip install streamlit- 
+#venv\Scripts\activate
+
+
+
+
+import streamlit as st
+st.title("SRM EASWARI ENGINEERING COLLEGE")
+num1=st.text_input("enter the number")
+num2=st.text_input("enter num 2")
+
+if st.button("add"):
+    st
+                       
